c26625394357afa99263f575c36275991c63a120
jyoshnasavvy <100415342+jyoshnasavvy@users.noreply.github.com>
2022-02-28 23:33:37 +0530

    Add files via upload

---
+++ sakila database.sql
@@ -0,0 +1,29 @@
+select * from sakila.film;
+select title, release_year from sakila.film;
+select release_year from sakila.film;
+select title from sakila.film;
+select * from sakila.film;
+select rating from sakila.film;
+select length from sakila.film;
+select replacement_cost from sakila.film;
+select description from sakila.film;
+select original_language_id from sakila.film;
+select rental_during from sakila.film;
+select last_update from sakila.film;
+select special_features from sakila.film;
+select film_id from sakila.film;
+select film_list from sakila.film;
+select year(2006) from sakila.film;
+select year(2008) from sakila.film;
+select year(2009) from sakila.film;
+select length(86) from sakila.film;
+select length(76) from sakila.film;
+select length(65) from sakila.film;
+select tength(55) from sakila.film;
+select length(45) from sakila.film;
+select count(4) from sakila.film;
+select count(6) from sakila.film;
+
+
+
+
